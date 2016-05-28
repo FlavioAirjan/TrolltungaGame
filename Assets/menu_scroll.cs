da7de76298b4877ab9615d7e17ac5fd040519674
Rezeck <rezeck@dcc.ufmg.br>
2016-05-27 21:21:17 -0300

    test

---
+++ Assets/menu_scroll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu_scroll : MonoBehaviour {
+	public float speed = 0.008f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (Time.time * speed, 0);
+		GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
