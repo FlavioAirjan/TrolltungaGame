6ce191c4593d7feff39e8d14f17ba768ffd72114
Rezeck <rezeck@dcc.ufmg.br>
2016-05-28 15:33:14 -0300

    Added into magpie game logo

---
+++ Assets/move.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public float speed = 0.02f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.right * Time.deltaTime * speed);
+	}
+}
