91ede6335c829bd9a212f6d60bb67216583aaf46
André Borba Netto Assis <Andr Borba Netto Assis>
2016-07-03 13:24:41 -0300

    DialogBoxSystem created and implemented.

---
+++ Assets/ImportText.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ImportText : MonoBehaviour {
+
+    public int limiteCaracteresPorLinha = 30;
+    [HideInInspector]
+    public string[] text;
+    private int currentLine;
+    List<string> list;
+   
+
+    // Use this for initialization
+    void Start()
+    {
+        currentLine = 0;
+        list = arrumaTamanhoFrases(text, limiteCaracteresPorLinha);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Se tiver texto.
+        if (text.Length > 0)
+        {
+
+            
+
+            if (currentLine == 0)
+            {
+                gameObject.GetComponent<Text>().text = list[currentLine];
+                currentLine++;
+            }
+
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                if (currentLine >= list.Count)
+                {
+                    text = null;
+                    currentLine = 0;
+                    gameObject.transform.parent.gameObject.SetActive(false);
+                   
+                }
+                else
+                {
+                    gameObject.GetComponent<Text>().text = list[currentLine];
+                    currentLine++;
+                }
+            }
+
+
+
+
+
+        }
+
+    }
+
+    //Essa função ajeita as frases para caber direito no dialogBox.
+    List<string> arrumaTamanhoFrases(string[] texts, int limitPerLine)
+    {
+
+        List<string> list = new List<string>();
+
+        int currentText = 0;
+        string text = texts[currentText];
+        
+        int countChar = 0;
+
+        string buffer = "";
+
+        int numOfLines = 0;
+        bool endOfText = false;
+
+
+
+        //Percorre a frase toda.
+        for (int i = 0; i < text.Length; i++)
+        {
+
+            //Toda vez que o quadro estiver todo preenchido.
+            if (numOfLines == 3 || endOfText)
+            {
+
+                text.Substring(i, 1);
+                
+                if (buffer != "" && buffer != "\n")
+                {
+                    list.Add(buffer);
+                    //gameObject.GetComponent<Text>().text = buffer;
+
+                }
+                ////Debug.Log(buffer);
+                buffer = "";
+
+                numOfLines = 0;
+                countChar = 0;
+
+                if (!endOfText)
+                {
+                    i--;
+                }
+                else
+                {
+                    
+                    //Pega o indice do próximo texto.
+                    currentText++;
+                    //Se o texto exisitir.
+
+                    if (currentText < texts.Length)
+                    {
+                        text = texts[currentText];
+                        ////Debug.Log(text);
+                        endOfText = false;
+                        i = -1;
+                    }
+                }
+
+            }
+            else
+            {
+                
+                if (countChar == limitPerLine || text.Substring(i, 1) == "\n")
+                {
+                    
+                    numOfLines++;
+                    countChar = 0;
+                    if (text.Substring(i, 1) != "\n")
+                    {
+                        countChar++;
+                        buffer += text.Substring(i, 1);
+                    }
+                }
+
+
+                if (numOfLines < 3)
+                {
+                    countChar++;
+                    buffer += text.Substring(i, 1);
+                }
+
+                if (i == text.Length - 1)
+                {
+                    endOfText = true;
+                    i--;
+                }
+
+            }
+
+        }
+
+
+
+
+
+        return list;
+    }
+}
---
+++ Assets/Speaking.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speaking : MonoBehaviour {
+
+
+    public string[] message;
+    public GameObject DialogBoxText;
+
+    // Use this for initialization
+    void Start () {
+        DialogBoxText.transform.parent.gameObject.SetActive(true);
+
+
+
+        DialogBoxText.GetComponent<ImportText>().text = message;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
